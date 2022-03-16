192fa00b41ac757a17eec2bd1a84a2b5a65699c7
Leeyounwoo <leeyw2709@gmail.com>
2022-03-16 09:44:03 +0900

    남은 데이터 크롤링 분담

--- 크롤링_지원님.py
+++ 크롤링_지원님.py
@@ -2,6 +2,8 @@ from datetime import date, timedelta
 import twint
 import csv
 import time
+import os.path
+
 
 
 # 크롤링 날짜 지정
@@ -17,7 +19,8 @@ end_date = date(2021,12,31)
 delta = timedelta(days=1)
 crolling_days = dete_range(start_date, end_date, delta)
 
-
+crolling_days = crolling_days[:-1]
+print(len(crolling_days), crolling_days[-1])
 # 라면 제품명 가져오기
 f = open('Ramen_nickname.csv', 'r', encoding='utf-8')
 rdr = csv.reader(f)
@@ -27,17 +30,15 @@ for line in rdr:
 f.close()
 
 
-column_name = ramen_data[0]
-ramen_names = []
-keys = []
-# 제품 개수
-for line in ramen_data[400:]:
-    if line[3] != line[4]:
-        ramen_names.append(line[4])
-        keys.append(line[11])
+# ramen_names = ['신림동 백순대 볶음면 컵', '간짬뽕 라면', '삼양 튀김칼국수 컵', '삼양 된장라면']
+# ramen_names = ['나가사끼짬뽕 큰컵', '쇠고기면 큰컵', '삼양 김치찌개 큰컵', '삼양라면 큰컵']
+# ramen_names = ['국민컵라면', '삼양 컵육개장', '삼양 고추짬뽕 컵', '나가사끼짬뽕 컵', '삼양라면 매운맛 컵']
+ramen_names = ['삼양라면 컵', '삼양라면 컵 오리지널', '삼계탕면 컵', '삼양라면 골드 컵', '미원라면 컵']
+# ramen_names = ['하이리빙파워 미니컵', '삼양 맛있는라면 컵', '삼양라면 매운맛 컵', '삼양 유부우동 큰컵']
 
-ramen_result = {key: {} for key in keys}
 
+print(len(ramen_names))
+print(ramen_names)
 for idx1 in range(len(ramen_names)):
     print(ramen_names[idx1])
     filename = ramen_names[idx1] + '_2016_0101_2021_1231.csv'
